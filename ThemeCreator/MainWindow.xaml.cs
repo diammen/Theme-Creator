951e2c29e9c91e4ae42516c51aabfa9d4549829a
Marcel Dao <marcelnamdao@gmail.com>
2019-02-28 18:01:25 -0800

    write to file added

--- ThemeCreator/MainWindow.xaml.cs
+++ ThemeCreator/MainWindow.xaml.cs
@@ -28,8 +28,10 @@ namespace ThemeCreator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string currentFilePath = "";
         private const string hexPrefix = @"#";
         private List<Expander> expList = new List<Expander>();
+        Theme theme = new Theme();
         public class Theme
         {
             private List<Scene> _scenes;
@@ -42,6 +44,7 @@ namespace ThemeCreator
                 _scenes.Add(new MainMenu());
                 _scenes.Add(new OptionsMenu());
                 _scenes.Add(new InGame());
+                _scenes.Add(new GameOverMenu());
             }
 
             public Theme(List<Scene> newScenes)
@@ -54,12 +57,26 @@ namespace ThemeCreator
         {
             private string HexCode;
             private FontFamily _font;
+            private Dictionary<string, string> _textColors;
 
             public string TextColor { get => HexCode; set => HexCode = value; }
 
             public Scene()
             {
                 Font = new FontFamily("Resources/Acme 7 Wide.ttf#Grixel Acme 7 Wide");
+            }
+
+            public Dictionary<string, string> TextColors
+            {
+                get
+                {
+                    return _textColors;
+                }
+                set
+                {
+                    _textColors = value;
+                    NotifyPropertyChanged("TextColors");
+                }
             }
 
             public FontFamily Font
@@ -104,34 +121,34 @@ namespace ThemeCreator
 
         public class InGame : Scene
         {
-            private string perfectColor;
-            private string greatColor;
-            private string missColor;
-            private string scoreColor;
-
-            private Dictionary<string, string> _textColors;
-
-            public Dictionary<string, string> TextColors { get => _textColors; set => _textColors = value; }
 
             public InGame()
             {
-                _textColors = new Dictionary<string, string>();
+                TextColors = new Dictionary<string, string>();
+
+                TextColors.Add("PERFECT", "#FFFFFF");
+                TextColors.Add("GREAT", "#FFFFFF");
+                TextColors.Add("MISS", "#FFFFFF");
+                TextColors.Add("SCORE", "#FFFFFF");
             }
         }
-        Theme test = new Theme();
+
+        public class GameOverMenu : Scene
+        {
+            public GameOverMenu()
+            {
+                TextColor = "FFFFFF";
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
 
-            menuTextColor.DataContext = test.Scenes[0];
-            optionsTextColor.DataContext = test.Scenes[1];
-            menuFont.DataContext = test.Scenes[0];
-            optionsFont.DataContext = test.Scenes[1];
-
             expList.Add(mainMenuExpand);
             expList.Add(optionsExpand);
             expList.Add(ingameExpand);
-    }
+            expList.Add(gameOverExpand);
+        }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
@@ -152,36 +169,39 @@ namespace ThemeCreator
 
             Nullable<bool> result = dlg.ShowDialog();
 
-            string input = "";
-
-            input += "Main Menu: " + menuTextColor.Text + "\n";
-            input += "Options Menu: " + optionsTextColor.Text + "\n";
-
             if (result == true)
             {
-                using (FileStream file = File.Create(dlg.FileName))
-                {
-                    byte[] info = new UTF8Encoding(true).GetBytes(input);
-                    file.Write(info, 0, info.Length);
-                }
+                FileStream temp = File.Create(dlg.FileName);
+                temp.Close();
+
+                currentFilePath = dlg.FileName;
+                theme = new Theme();
+                menuFont.DataContext = menuTextColor.DataContext = theme.Scenes[0];
+                optionsFont.DataContext = optionsTextColor.DataContext = theme.Scenes[1];
+                ingameFont.DataContext = perfectTextColor.DataContext = greatTextColor.DataContext = missTextColor.DataContext = scoreTextColor.DataContext = theme.Scenes[2];
+                gameOverFont.DataContext = gameOverTextColor.DataContext = theme.Scenes[3];
             }
         }
 
         private void TextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
-            if (!textBox.Text.StartsWith(hexPrefix))
+            if (textBox.Text.Length > 0)
             {
-                textBox.Text = hexPrefix + textBox.Text;
-                textBox.SelectionStart = textBox.Text.Length;
+                if (!textBox.Text.StartsWith(hexPrefix))
+                {
+                    textBox.Text = hexPrefix + textBox.Text;
+                    textBox.SelectionStart = textBox.Text.Length;
+                }
             }
         }
 
         private void SelectionChanged(object sender, RoutedEventArgs e)
         {
             TextBox textBox = (TextBox)sender;
-            if (textBox.SelectionStart <= 0)
-            textBox.SelectionStart = textBox.Text.Length;
+            if (textBox.Text.Length > 0)
+                if (textBox.SelectionStart <= 0)
+                    textBox.SelectionStart = textBox.Text.Length;
         }
 
         private void PreviewInput(object sender, TextCompositionEventArgs e)
@@ -190,8 +210,10 @@ namespace ThemeCreator
             Regex rgx = new Regex(pattern);
 
             TextBox textBox = (TextBox)sender;
-            if (!rgx.IsMatch(e.Text) || textBox.Text.Length >= 7)
-                e.Handled = true;
+
+            if (textBox.Text.Length > 0)
+                if (!rgx.IsMatch(e.Text) || textBox.Text.Length >= 7)
+                    e.Handled = true;
         }
 
         private void TextBoxLoaded(object sender, RoutedEventArgs e)
@@ -202,7 +224,44 @@ namespace ThemeCreator
 
         private void SaveFile_Click(object sender, RoutedEventArgs e)
         {
+            // store entered values into theme object
+            theme.Scenes[0].Font = new FontFamily(menuFont.Text);
+            theme.Scenes[0].TextColor = menuTextColor.Text;
+            theme.Scenes[1].Font = new FontFamily(optionsFont.Text);
+            theme.Scenes[1].TextColor = optionsTextColor.Text;
+            theme.Scenes[2].Font = new FontFamily(ingameFont.Text);
+            theme.Scenes[2].TextColors["PERFECT"] = perfectTextColor.Text;
+            theme.Scenes[2].TextColors["GREAT"] = greatTextColor.Text;
+            theme.Scenes[2].TextColors["MISS"] = missTextColor.Text;
+            theme.Scenes[2].TextColors["SCORE"] = scoreTextColor.Text;
+            theme.Scenes[3].Font = new FontFamily(gameOverFont.Text);
+            theme.Scenes[3].TextColor = gameOverTextColor.Text;
 
+            string pattern = @"(Resources/[\w+\s+.ttf]+)";
+            Regex rgx = new Regex(pattern);
+
+            string input = "";
+
+            input += "@Main Menu; font, font color\n";
+            input += rgx.Match(theme.Scenes[0].Font.ToString()).ToString() + "\n";
+            input += theme.Scenes[0].TextColor + "\n\n";
+            input += "@Options Menu; font, font color\n";
+            input += rgx.Match(theme.Scenes[1].Font.ToString()).ToString() + "\n";
+            input += theme.Scenes[1].TextColor + "\n\n";
+            input += "@In-game; font, perfect color, great color, miss color, score color\n";
+            input += rgx.Match(theme.Scenes[2].Font.ToString()).ToString() + "\n";
+            foreach (string color in theme.Scenes[2].TextColors.Values)
+                input += color + "\n";
+            input += "\n";
+            input += "@Game Over Menu; font, font color\n";
+            input += rgx.Match(theme.Scenes[3].Font.ToString()).ToString() + "\n";
+            input += theme.Scenes[3].TextColor;
+
+            using (FileStream file = File.OpenWrite(currentFilePath))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(input);
+                file.Write(info, 0, info.Length);
+            }
         }
 
         private void mainFontBrowse_Click(object sender, RoutedEventArgs e)
@@ -217,7 +276,7 @@ namespace ThemeCreator
 
                 foreach(FontFamily fontFam in fonts)
                 {
-                    test.Scenes[0].Font = fontFam;
+                    theme.Scenes[0].Font = fontFam;
                 }
                 BindingExpression binding = menuFont.GetBindingExpression(TextBox.TextProperty);
                 binding.UpdateSource();
@@ -235,7 +294,7 @@ namespace ThemeCreator
 
                 foreach (FontFamily fontFam in fonts)
                 {
-                    test.Scenes[1].Font = fontFam;
+                    theme.Scenes[1].Font = fontFam;
                 }
                 BindingExpression binding = menuFont.GetBindingExpression(TextBox.TextProperty);
                 binding.UpdateSource();
@@ -253,7 +312,25 @@ namespace ThemeCreator
 
                 foreach (FontFamily fontFam in fonts)
                 {
-                    test.Scenes[2].Font = fontFam;
+                    theme.Scenes[2].Font = fontFam;
+                }
+                BindingExpression binding = menuFont.GetBindingExpression(TextBox.TextProperty);
+                binding.UpdateSource();
+            }
+        }
+        private void gameOverFontBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "True Type Fonts (.ttf)|*.ttf";
+
+            if (dlg.ShowDialog() == true)
+            {
+
+                var fonts = Fonts.GetFontFamilies(dlg.FileName);
+
+                foreach (FontFamily fontFam in fonts)
+                {
+                    theme.Scenes[3].Font = fontFam;
                 }
                 BindingExpression binding = menuFont.GetBindingExpression(TextBox.TextProperty);
                 binding.UpdateSource();
@@ -274,6 +351,31 @@ namespace ThemeCreator
                     i.IsExpanded = false;
                 }
             }
+        }
+
+        private void perfectColorGotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox input = (TextBox)sender;
+            Binding binding = new Binding("Text");
+            binding.Source = input;
+            BindingOperations.SetBinding(timing, TextBlock.ForegroundProperty, binding);
+            timing.Text = "PERFECT";
+        }
+        private void greatColorGotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox input = (TextBox)sender;
+            Binding binding = new Binding("Text");
+            binding.Source = input;
+            BindingOperations.SetBinding(timing, TextBlock.ForegroundProperty, binding);
+            timing.Text = "GREAT";
+        }
+        private void missColorGotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox input = (TextBox)sender;
+            Binding binding = new Binding("Text");
+            binding.Source = input;
+            BindingOperations.SetBinding(timing, TextBlock.ForegroundProperty, binding);
+            timing.Text = "MISS";
         }
     }
 }
